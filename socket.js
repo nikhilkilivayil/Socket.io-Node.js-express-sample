e639f33074ca1e1751396fecf364da70563ddbe2
Nikhil Kilivayil <nikhilkilivayil@gmail.com>
2016-11-08 00:31:35 +0530

    Socket.io connected

---
+++ socket.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nikhil on 7/11/16.
+ */
+var app=require('./app');
+var io=require('socket.io');
+var socket=new io();
+
+socket.on('connection',function (socket) {
+    console.log('connected');
+    socket.emit('news', { hello: 'world' });
+   /* socket.on('my other event', function (data) {
+        console.log(data);*/
+});
+module.exports=socket;
